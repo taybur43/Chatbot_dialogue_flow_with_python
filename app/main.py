ad04c189d5511251c42112096f1d9ff4caf875d1
taybur.rahaman <sagor.ece@gmail.ocm>
2019-11-24 11:01:21 +0600

    Testing with python

---
+++ app/main.py
@@ -0,0 +1,4 @@
+import os
+print("Hello BOT")
+print("who the hell are you not doing this")
+print("testing...")
--- main.py
+++
@@ -1,3 +0,0 @@
-import os
-print("Hello BOT")
-print("who the hell are you not doing this")
